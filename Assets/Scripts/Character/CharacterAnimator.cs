3924734636019dd61be7caca89b7333332d37ba9
Key <tavik002@gmail.com>
2023-02-04 01:41:21 +0800

    animator wip

---
+++ Assets/Scripts/Character/CharacterAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    private Animator _animator = null;
+
+    public Animator Animator
+    {
+        get
+        {
+            if (_animator == null)
+            {
+                _animator = GetComponent<Animator>();
+            }
+
+            return _animator;
+        }
+    }
+
+
+    private int _moveSpeed = Animator.StringToHash("MoveSpeed");
+
+    public void SetMoveSpeed(float moveSpeed)
+    {
+        Animator.SetFloat(_moveSpeed, moveSpeed);
+    }
+}
--- Assets/Scripts/Character/CharacterMovementController.cs
+++ Assets/Scripts/Character/CharacterMovementController.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CharacterMovementController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private bool _isHorizontalReverse = false;
     [SerializeField] private float _jumpForce;
     [SerializeField] private Rigidbody _rigidbody;
 
+    private CharacterAnimator _characterAnimator;
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _characterAnimator = GetComponentInChildren<CharacterAnimator>();
     }
 
     private void Update()
@@ -20,7 +23,15 @@ public class PlayerController : MonoBehaviour
         {
             horizontalSpeed = -horizontalSpeed;
         }
+
         _rigidbody.velocity = new Vector3(horizontalSpeed, _rigidbody.velocity.y, 0f);
+        if (Mathf.Abs(horizontalSpeed) > 0.01f)
+        {
+            transform.rotation =
+                Quaternion.LookRotation(horizontalSpeed > 0f ? Vector3.right : Vector3.left, Vector3.up);
+        }
+
+        _characterAnimator.SetMoveSpeed(Mathf.Abs(_rigidbody.velocity.x));
 
         if (Input.GetButtonDown("Jump"))
         {
