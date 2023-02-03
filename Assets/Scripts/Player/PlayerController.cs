e305ebcd496bb00559a4426a6055a60b3bbb79f7
Key <tavik002@gmail.com>
2023-02-03 23:23:11 +0800

    player controller wip

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rigidbody.AddForce(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rigidbody.AddForce(Vector3.right);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddForce(Vector3.up);
+        }
+
+    }
+}
