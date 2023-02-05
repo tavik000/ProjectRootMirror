cdb375751f3407b6e50435c3603e8a5e4102be7f
Key <tavik002@gmail.com>
2023-02-05 12:13:43 +0800

    Title Scene

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField] private Button _startBtn;
+
+    private void Awake()
+    {
+        _startBtn.onClick.AddListener(OnClickStartButton);
+    }
+
+    private static void OnClickStartButton()
+    {
+        GameSceneManager.GoNextLevel();
+    }
+}
